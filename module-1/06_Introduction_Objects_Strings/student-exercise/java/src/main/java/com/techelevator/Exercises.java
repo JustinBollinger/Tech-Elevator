a05494c02879db884434712adfd82261d31be82e
Justin Bollinger <jbollingerinterpreter@gmail.com>
2020-12-08 18:05:07 -0500

    Checkpoint 12/8 6:05pm

--- module-1/06_Introduction_Objects_Strings/student-exercise/java/src/main/java/com/techelevator/Exercises.java
+++ module-1/06_Introduction_Objects_Strings/student-exercise/java/src/main/java/com/techelevator/Exercises.java
@@ -133,8 +133,11 @@ public class Exercises
 	
 	/*
 	 * Given a string, return a version without the first and last char, so "Hello"
-	 * yields "ell". The string length will be at least 2. withoutEnd("Hello") →
-	 * "ell" withoutEnd("java") → "av" withoutEnd("coding") → "odin"
+	 * yields "ell".The string length will be at least 2.
+	 * 
+	 * withoutEnd("Hello") => "ell"
+	 * withoutEnd("java") => "av"
+	 * withoutEnd("coding") => "odin"
 	 */
 	public String withoutEnd(String str)
 	{
