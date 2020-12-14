b37e2c72db20515c9d2d953d9ab4fd5716eb6147
Justin Bollinger <jbollingerinterpreter@gmail.com>
2020-12-14 16:33:57 -0500

    Checkpoint 12/14 4:33pm

--- module-1/09_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Television.java
+++ module-1/09_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Television.java
@@ -1,5 +1,35 @@
 package com.techelevator;
 
-public class Television {
-
+public class Television
+{
+	private boolean isOn = false;
+	private int currentChannel = 3;
+	private int currentVolume = 2;
+	
+	public boolean isOn()
+	{
+		return this.isOn;
+	}
+	
+	
+	public void channelUp()
+	{
+		if(this.isOn)
+		{
+			if(this.currentChannel < 18)
+			{
+				this.currentChannel++;
+			}
+			else 
+			{
+				this.currentChannel = 3;	// 3 because the requirements say to deal with channels between 3-18.
+			}
+		}
+	}
+	
+	public void changeChannel(int currentChannel)
+	{
+		this.currentChannel = currentChannel;
+	}
+	
 }
