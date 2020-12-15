2070f06e8d66a9dfe1c5848091b6de022645f38f
Justin Bollinger <jbollingerinterpreter@gmail.com>
2020-12-15 13:29:57 -0500

    Checkpoint to avoid merge conflicts.

--- module-1/12_Polymorphism/student-lecture/java/src/main/java/com/techelevator/farm/FarmAnimal.java
+++ module-1/12_Polymorphism/student-lecture/java/src/main/java/com/techelevator/farm/FarmAnimal.java
@@ -8,6 +8,20 @@ public class FarmAnimal {
 		this.name = name;
 		this.sound = sound;
 	}
+	
+//	// overloading the constructor
+//	// allows the user to create a FarmAnimal in 2 different ways
+//	public FarmAnimal()
+//	{
+//		// call a different constructor
+//		// with a different signature...
+//		this("Cow", "moo!");
+//		
+//		//... so that I don't repeat myself.
+//		// below code is not very DRY
+////		this.name = "Cow";
+////		this.sound = "moo!";
+//	}
 
 	public String getName() {
 		return name;
