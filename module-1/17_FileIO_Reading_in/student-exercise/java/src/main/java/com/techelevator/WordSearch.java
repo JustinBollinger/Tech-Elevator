ca69733c10859e623d05e07ffb2864ed40d04837
Justin Bollinger <jbollingerinterpreter@gmail.com>
2021-01-04 22:47:58 -0500

    checkpoint

--- module-1/17_FileIO_Reading_in/student-exercise/java/src/main/java/com/techelevator/WordSearch.java
+++ module-1/17_FileIO_Reading_in/student-exercise/java/src/main/java/com/techelevator/WordSearch.java
@@ -1,9 +1,10 @@
 package com.techelevator;
 
-public class WordSearch {
+public class WordSearch
+{
 
-	public static void main(String[] args) {
-	
+	public static void main(String[] args)
+	{
 
 	}
 
