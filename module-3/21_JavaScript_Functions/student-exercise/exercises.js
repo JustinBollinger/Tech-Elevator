05e8acc7b10e42b90e4928486457d95e44aad0f9
Justin Bollinger <jbollingerinterpreter@gmail.com>
2021-02-17 20:30:10 -0500

    Submitting JavaScript Functions Exercises

--- module-3/21_JavaScript_Functions/student-exercise/exercises.js
+++ module-3/21_JavaScript_Functions/student-exercise/exercises.js
@@ -19,15 +19,29 @@
  * @param {boolean} [recommendation=false] does the student have a recommendation
  * @returns {boolean} true if they are admitted
  */
+    function isAdmitted(gpa, satScore = 0, recommendation = false){
+        if(gpa > 4.0 || satScore > 1300 || (gpa > 3.0 && recommendation == true) || (satScore > 1200 && recommendation == true))
+        {
+            return true;
+        }
+        return false;
+    }
+
 
 /**
  * Write a function called useParameterToFilterArray that takes an anonymous
- * function and uses that in the `unfilteredArray` filter function. Return the result.
+ * function and uses that in the `unfilteredArray` filter function.
+ * Return the result.
  *
  * @param {function} filterFunction the function to filter with
  * @returns {number[]} the filtered array
  */
-let unfilteredArray = [1, 2, 3, 4, 5, 6];
+    let unfilteredArray = [1, 2, 3, 4, 5, 6];
+    function useParameterToFilterArray(filterFunction){
+        return unfilteredArray.filter(
+            (number) => {return filterFunction(number);}
+        );
+ }
 
 /**
  * Write a function called makeNumber that takes two strings
@@ -42,6 +56,11 @@ let unfilteredArray = [1, 2, 3, 4, 5, 6];
  * @returns {number} the resultant number
  */
 
+ function makeNumber(first, second=''){
+     return parseInt(first+second);
+ }
+
+
 /**
  * Write a function called addAll that takes an unknown number of parameters
  * and adds all of them together. Return the sum.
@@ -49,12 +68,33 @@ let unfilteredArray = [1, 2, 3, 4, 5, 6];
  * @param {...number} num a series of numbers to add together
  * @returns {number} the sum of all the parameters (or arguments)
  */
+ function addAll(){
+    let sum = 0;
+    for(let i = 0; i < arguments.length; i++){
+        sum += arguments[i];
+    }
+    return sum;
+ }
+
 
 /*
  * Write and document a function called makeHappy that takes
  * an array and prepends 'Happy ' to the beginning of all the
  * words and returns them as a new array. Use the `map` function.
  */
+
+ /**
+  * Takes the word that is passed, prepends 'Happy ' to it,
+  * returns the new string.
+  * 
+  * @param {string} words: word to which 'Happy ' will be prepended
+  * @returns {string} the word with 'Happy ' prepended on
+  */
+    function makeHappy(words)
+    {
+        return words.map(word => 'Happy ' + word);
+    }
+
 
 /*
  * Write and document a function called getFullAddressesOfProperties
@@ -74,12 +114,59 @@ let unfilteredArray = [1, 2, 3, 4, 5, 6];
  * Use `map` and an anonymous function.
  */
 
+ /**
+  * Takes an array of objects and converts the keys within to useful strings
+  * and then organizes them in a readable single-line format in the following order:
+  *     -> streetNumber streetName streetType city state zip
+  * 
+  * 
+  * @param {Object} addressesArray array of multiple JS objects containing keys
+  * @returns {string} one single array that contains each element of an object separated into strings
+  */
+    function getFullAddressesOfProperties(addressesArray){
+
+        let address = addressesArray.map((object) =>
+        {
+            return object.streetNumber + ' ' + object.streetName + ' ' + object.streetType + ' '
+                 + object.city + ' ' + object.state + ' ' + object.zip;
+        });
+
+        return address;
+    }
+
+
+
 /*
  * Write and document a function called findLargest.
  *
  * Using `forEach`, find the largest element in an array.
  * It must work for strings and numbers.
  */
+   
+ /**
+  * Uses a forEach loop to loop through an array of numbers or strings
+  * in order to select the largest element, either a number or string respectively.
+  * 
+  * 
+  * @param {number} array of numbers
+  * - OR -
+  * @param {string} array of strings
+  * @returns {number} OR {string} the largest value of either type relative
+  * to what is passed as the argument.
+  */
+    function findLargest(array){
+       
+        let largest = array[0];
+        array.forEach((element) => 
+        {
+            if(element > largest)
+            {
+                largest = element;
+            }
+        });
+        return largest;
+    }
+
 
 /*
  * CHALLENGE
