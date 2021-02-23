a0731b5d2d4b209aa57ddd1483aed12c134ff4b8
Justin Bollinger <jbollingerinterpreter@gmail.com>
2021-02-23 14:03:02 -0500

    Checkpoint

--- module-3/31_Vue_Event_Handling/student-lecture/src/components/ProductReview.vue
+++ module-3/31_Vue_Event_Handling/student-lecture/src/components/ProductReview.vue
@@ -4,33 +4,68 @@
 
     <p class="description">{{ description }}</p>
 
+    <a v-on:click.prevent="showForm"
+      v-if="formVisible === false">Show Form </a>
+    <a v-on:click.prevent="clearForm"
+      v-if="formVisible">Hide Form</a>
+
+    <form v-on:submit.prevent="addReview"
+          v-if="formVisible"
+    >
+      <div class="form-element">
+        <label for="reviewer">Name:</label>
+        <input type="text" v-model="newReview.reviewer">
+      </div>
+      <div class="form-element">
+        <label for="title">Title:</label>
+        <input type="text" v-model="newReview.title">
+      </div>
+      <div class="form-element">
+        <label for="rating">Rating:</label>
+        <select v-model="newReview.rating">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+      <div class="form-element">
+        <label for="review">Review:</label>
+        <textarea v-model="newReview.review"></textarea>
+      </div>
+
+      <input type="submit" value="Save">
+      <input type="button" value="Cancel">
+    </form>
+
     <div class="well-display">
-      <div class="well">
+      <div class="well" v-on:click="setFilter(0)">
         <span class="amount">{{ averageRating }}</span>
         Average Rating
       </div>
 
-      <div class="well">
+      <div class="well" v-on:click="setFilter(1)">
         <span class="amount">{{ numberOfOneStarReviews }}</span>
         1 Star Review{{ numberOfOneStarReviews === 1 ? '' : 's' }}
       </div>
 
-      <div class="well">
+      <div class="well" v-on:click="setFilter(2)">
         <span class="amount">{{ numberOfTwoStarReviews }}</span>
         2 Star Review{{ numberOfTwoStarReviews === 1 ? '' : 's' }}
       </div>
 
-      <div class="well">
+      <div class="well" v-on:click="setFilter(3)">
         <span class="amount">{{ numberOfThreeStarReviews }}</span>
         3 Star Review{{ numberOfThreeStarReviews === 1 ? '' : 's' }}
       </div>
 
-      <div class="well">
+      <div class="well" v-on:click="setFilter(4)">
         <span class="amount">{{ numberOfFourStarReviews }}</span>
         4 Star Review{{ numberOfFourStarReviews === 1 ? '' : 's' }}
       </div>
 
-      <div class="well">
+      <div class="well" v-on:click="setFilter(5)">
         <span class="amount">{{ numberOfFiveStarReviews }}</span>
         5 Star Review{{ numberOfFiveStarReviews === 1 ? '' : 's' }}
       </div>
@@ -39,7 +74,7 @@
     <div
       class="review"
       v-bind:class="{ favorited: review.favorited }"
-      v-for="review in reviews"
+      v-for="review in filteredReviews"
       v-bind:key="review.id"
     >
       <h4>{{ review.reviewer }}</h4>
@@ -68,10 +103,14 @@
 export default {
   name: "product-review",
   data() {
+    // this is what the "object" KNOWS
+    // the state of the object
     return {
       name: "Cigar Parties for Dummies",
       description:
         "Host and plan the perfect cigar party for all of your squirrelly friends.",
+      formVisible: false,
+      filter: 0,
       newReview: {},
       reviews: [
         {
@@ -110,6 +149,8 @@ export default {
     };
   },
   computed: {
+    // these are 100% read-only properties
+    // act like getters in Java
     averageRating() {
       let sum = this.reviews.reduce((currentSum, review) => {
         return currentSum + review.rating;
@@ -117,29 +158,43 @@ export default {
       return (sum / this.reviews.length).toFixed(2);
     },
     numberOfOneStarReviews() {
-      return this.reviews.reduce((currentCount, review) => {
-        return currentCount + (review.rating === 1);
-      }, 0);
+      return this.numberOfReviews(1);
     },
     numberOfTwoStarReviews() {
-      return this.reviews.reduce((currentCount, review) => {
-        return currentCount + (review.rating === 2);
-      }, 0);
+      return this.numberOfReviews(2);
     },
     numberOfThreeStarReviews() {
-      return this.reviews.reduce((currentCount, review) => {
-        return currentCount + (review.rating === 3);
-      }, 0);
+      return this.numberOfReviews(3);
     },
     numberOfFourStarReviews() {
-      return this.reviews.reduce((currentCount, review) => {
-        return currentCount + (review.rating === 4);
-      }, 0);
+      return this.numberOfReviews(4);
     },
     numberOfFiveStarReviews() {
-      return this.reviews.reduce((currentCount, review) => {
-        return currentCount + (review.rating === 5);
-      }, 0);
+      return this.numberOfReviews(5);
+    },
+    filteredReviews(){
+      return this.reviews.filter(review => this.filter === 0 ? true : review.rating === this.filter);
+    }
+  },
+  methods: {
+    // what the object can DO
+    addReview(){
+      this.newReview.rating = parseInt(this.newReview.rating);
+      this.reviews.unshift(this.newReview);
+      this.clearForm();
+    },
+    clearForm(){
+      this.newReview = {};
+      this.formVisible = false;
+    },
+    showForm(){
+      this.formVisible = true;
+    },
+    setFilter(value) {
+      this.filter = value;
+    },
+    numberOfReviews(stars) {
+      return this.reviews.filter(review => review.rating === stars).length;
     }
   }
 };
@@ -225,5 +280,12 @@ form > input[type=submit] {
   width: 100px;
   margin-right: 10px;
 }
+.well, .well span {
+  cursor: pointer;
+}
+.well:hover {
+  background-color: lightgoldenrodyellow;
+}
+
 </style>
 
